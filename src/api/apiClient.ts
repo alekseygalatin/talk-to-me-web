28997ae14bbd75912f57ca122485e021bd4997b6
Oleg <shishkin_o@iuca.kg>
2024-12-19 17:15:13 +0600

    header and sidebar returned to chat

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import AuthService from "../core/auth/authService";
 
 const apiClient = axios.create({
-  //baseURL: "https://localhost:7099/api",
-  baseURL: "https://w9urvqhqc6.execute-api.us-east-1.amazonaws.com/Prod/api",
+  baseURL: "https://localhost:7099/api",
+  //baseURL: "https://w9urvqhqc6.execute-api.us-east-1.amazonaws.com/Prod/api",
   headers: {
     "Authorization": `${AuthService.getToken()}`,
     "Content-Type": "application/json", 
--- src/models/ProfileSettings.ts
+++ src/models/ProfileSettings.ts
@@ -1,6 +1,5 @@
 export interface ProfileSettings {
     theme: 'light' | 'dark';
-    language: string;
     volume: number;
     microphoneSensitivity: number;
   }
