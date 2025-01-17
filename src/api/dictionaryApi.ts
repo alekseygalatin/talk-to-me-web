67f9855fb3cec243dac0c7a44cf3fcdae6c5464e
Oleg <shishkin_o@iuca.kg>
2025-01-17 22:53:16 +0600

    dictionary page finished

--- src/api/dictionaryApi.ts
+++ src/api/dictionaryApi.ts
@@ -4,7 +4,7 @@ import apiClient from "./apiClient";
 export const addWordToDictionary = async (language: string, word: Word) => {
     word.language = language;
     const response = await apiClient.post(`/words`, word);
-    return response.data;
+    return response;
 }
 
 export const getWords = async (language: string): Promise<Word[]> => {
@@ -12,4 +12,13 @@ export const getWords = async (language: string): Promise<Word[]> => {
     return response.data;
 }
   
+export const deleteWord = async (langauge: string, word: string) => {
+    const response = await apiClient.delete(`/words/${langauge}/${word}`,);
+    return response;
+}
+
+export const setIncludeIntoChat = async (langauge: string, word: string, includeIntoChat: boolean) => {
+    const response = await apiClient.put(`/words/${langauge}/${word}/${includeIntoChat}`,);
+    return response;
+}
   
