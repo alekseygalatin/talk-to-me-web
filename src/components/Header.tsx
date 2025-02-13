645e8a66db8fef84384f250c01a95298a523e928
Oleg <shishkin_o@iuca.kg>
2025-02-13 19:20:12 +0600

    header menu is fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,22 +2,26 @@ import { Link, useNavigate } from 'react-router-dom';
 import {
     UserCog,
     Globe,
-    Menu,
+    Menu as MenuIcon,
     X,
     ChevronDown,
     LogOut,
+    Settings,
+    BookMarked,
+    LayoutDashboard
   } from 'lucide-react';
 import { useState } from 'react';
 import { useAppContext } from '../contexts/AppContext';
 import { Auth } from 'aws-amplify';
-
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import { NavLink } from "react-router-dom";
 
 const Header: React.FC = () => {
 
   const navigate = useNavigate();
-  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
   const { preferences, currentLanguage, isInitialized } = useAppContext();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const {theme, toggleTheme} = useAppContext();
 
     return (
         <header className="bg-white dark:bg-gray-800 shadow-sm">
@@ -35,27 +39,21 @@ const Header: React.FC = () => {
                 </Link>
               </div>
               {/* Desktop Navigation */}
-              <div className="hidden md:flex md:items-center md:space-x-6 ml-8">
-                <Link
+              <div className="hidden md:flex md:items-center md:space-x-4 ml-8">
+                <NavLink
                   to="/select-partner"
-                  className="text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-white text-sm font-medium"
+                  className={({isActive}) => `flex items-center gap-1 p-3 text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-white text-sm font-medium ${isActive ? "bg-gray-100 dark:bg-gray-700" : ""}`}
                 >
-                  Home
-                </Link>
+                  <LayoutDashboard className="w-5 h-5" /> Dashboard
+                </NavLink>
                 { isInitialized ? (
                   <>
-                    <Link
+                    <NavLink
                     to="/words"
-                      className="text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-white text-sm font-medium"
+                    className={({isActive}) => `flex items-center gap-1 p-3 text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-white text-sm font-medium ${isActive ? "bg-gray-100 dark:bg-gray-700" : ""}`}
                     >
-                      Dictionary
-                    </Link>
-                    <Link
-                      to="/select-language-to-learn"
-                      className="text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-white text-sm font-medium"
-                    >
-                      Languages
-                    </Link>
+                      <BookMarked className="w-5 h-5" /> Dictionary
+                    </NavLink>
                   </>
                 ) : ('') }
                 
@@ -63,67 +61,86 @@ const Header: React.FC = () => {
             </div>
             {/* Right Side: User Menu */}
             <div className="flex items-center">
-              {/* Language Selector */}
-              { currentLanguage ? (
-                <div className="hidden md:flex items-center">
-                <Link
-                  to="/select-language-to-learn"
-                  className="flex items-center text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-white"
-                >
-                  <Globe className="w-5 h-5 mr-1" />
-                  <span className="text-sm font-medium">
-                    {currentLanguage?.name}
-                  </span>
-                </Link>
-              </div>
-              ):('')}
-              
-              {/* User Dropdown */}
-              { isInitialized ? (
-                  <div className="relative ml-4">
-                  <button
-                    onClick={() => setIsUserMenuOpen(!isUserMenuOpen)}
-                    className="flex items-center text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-white focus:outline-none"
+                {/* User Dropdown */}
+          { isInitialized && (
+                <Menu>
+                  <MenuButton
+                    className="hidden md:flex items-center text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-white focus:outline-none"
                   >
-                    <UserCog className="w-6 h-6" />
+                    <Settings className="w-5 h-5" />
                     <span className="ml-2 text-sm font-medium">
                       {preferences?.name}
                     </span>
                     <ChevronDown className="w-4 h-4 ml-1" />
-                  </button>
-                  {isUserMenuOpen && (
-                    <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white dark:bg-gray-700 ring-1 ring-black ring-opacity-5">
-                      <div className="py-1">
+                  </MenuButton>
+                  
+                    <MenuItems className={"rounded-md shadow-lg bg-white dark:bg-gray-700 ring-1 ring-black ring-opacity-5 mt-2"} anchor="bottom start">
+                    
+                        <MenuItem>
                         <Link
                           to="/user-preferences"
-                          className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
-                          onClick={() => setIsUserMenuOpen(!isUserMenuOpen)}
+                          className="flex items-center gap-2 block px-6 pt-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
                         >
-                          Profile Settings
+                          <UserCog className="w-5 h-5" /> Profile Settings
                         </Link>
-                        <Link
-                          to="/select-language-to-learn"
-                          className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
-                          onClick={() => setIsUserMenuOpen(!isUserMenuOpen)}
-                        >
-                          Change Language
-                        </Link>
+                        </MenuItem>
+                        {/* Language Selector */}
+                        { currentLanguage && (
+                          
+                          <MenuItem>
+                          <Link
+                            to="/select-language-to-learn"
+                            className="flex items-center gap-2 block px-6 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
+                          >
+                            <Globe className="w-5 h-5" />
+                            {currentLanguage?.name} (change)
+                          </Link>
+                          </MenuItem>
+                       
+                        )}
+                        
+                        <MenuItem>
                         <button
                           onClick={() => {
                             Auth.signOut();
                             navigate("/login");
                           }}
-                          className="w-full text-left block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
-                          
+                          className="flex items-center gap-2 w-full px-6 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
                         >
+                          <LogOut className="w-5 h-5" />
                           Log Out
                         </button>
-                      </div>
-                    </div>
-                  )}
-                </div>
-              ):("")}
-              
+                        </MenuItem>
+                        <hr className='mt-2'/>
+                        <div className='flex items-center gap-2 px-6 py-4 text-sm text-gray-700 dark:text-gray-200'>
+                          <div className='inline-block'>
+                            <button
+                              onClick={toggleTheme}
+                              className="relative flex items-center px-2 py-1 w-10 h-5 rounded-full bg-gray-200 dark:bg-gray-600 transition-colors"
+                            >
+                              <span
+                                className={`absolute top-0 left-0 w-5 h-5 bg-gray-400 rounded-full transition-transform ${
+                                  theme === 'dark' ? 'transform translate-x-5 bg-blue-600' : 'transform translate-x-0'
+                                }`}
+                              ></span>
+                              <span
+                                className={`absolute w-full flex items-center justify-between px-2 text-xs font-medium uppercase ${
+                                  theme === 'dark' ? 'text-gray-300' : 'text-gray-600'
+                                }`}
+                              >
+                              </span>
+                            </button>
+                          </div>
+                          <div className='flex items-center'>
+                            <span>Dark mode</span>
+                          </div>
+                        </div>
+                    </MenuItems>
+                  
+                </Menu>
+                
+              )}
+            
               {/* Mobile Menu Button */}
               <div className="md:hidden ml-2 flex items-center">
                 <button
@@ -133,39 +150,33 @@ const Header: React.FC = () => {
                   {isMenuOpen ? (
                     <X className="block h-6 w-6 text-gray-700 dark:text-gray-300" />
                   ) : (
-                    <Menu className="block h-6 w-6 text-gray-700 dark:text-gray-300" />
+                    <MenuIcon className="block h-6 w-6 text-gray-700 dark:text-gray-300" />
                   )}
                 </button>
               </div>
             </div>
           </div>
         </div>
-
+          
         {/* Mobile Menu */}
         {isMenuOpen && (
           <div className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              <Link
+              <NavLink
                 to="/select-partner"
-                className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-50 dark:text-gray-300 dark:hover:text-white dark:hover:bg-gray-700"
+                className={({isActive}) => `flex items-center gap-2 px-3 py-3 text-base font-medium text-gray-700 dark:text-gray-300 ${isActive ? "bg-gray-100 dark:bg-gray-700" : ""}`}
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
               >
-                Home
-              </Link>
-              <Link
+                <LayoutDashboard className="w-5 h-5" /> Dashboard
+              </NavLink>
+              <NavLink
                 to="/words"
                 className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-50 dark:text-gray-300 dark:hover:text-white dark:hover:bg-gray-700"
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
               >
                 Dictionary
-              </Link>
-              <Link
-                to="/select-language-to-learn"
-                className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-50 dark:text-gray-300 dark:hover:text-white dark:hover:bg-gray-700"
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
-              >
-                Languages
-              </Link>
+              </NavLink>
+ 
               {/* User Menu */}
               <div className="mt-3 border-t border-gray-200 dark:border-gray-700 pt-4">
                 <div className="px-3">
